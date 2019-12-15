2fb5d5fff8aed3746f884f331fc2636999371141
JensWin <jenswindisch@gmx.de>
2019-12-15 16:20:25 +0100

    basket added

---
+++ basket/basket/basket/Controllers/CustomerBasketController.cs
@@ -0,0 +1,102 @@
+﻿using basket.Data;
+using basket.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace basket.Controllers
+{
+    [Route("Basket/")]
+    [ApiController]
+    public class CustomerBasketController : ControllerBase
+    {
+        private readonly BasketDbContext _context;
+
+        public CustomerBasketController(BasketDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /Basket
+        [HttpGet]
+        public string GetBaskets()
+        {
+            //return await _context.Baskets.ToListAsync();
+
+            return "Hallo";
+        }
+
+        /// <summary>
+        /// Gets the basket of a customer by the customerId
+        /// </summary>
+        /// <param name="id">CustomerId</param>
+        /// <returns></returns>
+        // GET: Basket/1
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CustomerBasket>> GetCustomerBasket(int id)
+        {
+            var customerBasket = await _context.Baskets.Include(b => b.Items).FirstOrDefaultAsync(b => b.CustomerId == id);
+
+            if (customerBasket == null)
+            {
+                return NotFound();
+            }
+
+            return customerBasket;
+        }
+
+        //POST: Basket/1
+        [HttpPost("{customerId}")]
+        public async Task<ActionResult<CustomerBasket>> AddItemToBasket(int customerId, [FromBody]BasketItem item)
+        {
+            var customerBasket = await _context.Baskets.Include(b => b.Items).FirstOrDefaultAsync(b => b.CustomerId == customerId);
+
+            //if there is no basket for the desired customer, create it
+            if (customerBasket == null)
+            {
+                customerBasket = new CustomerBasket { CustomerId = customerId, Items = new List<BasketItem>() };
+                _context.Baskets.Add(customerBasket);
+                await _context.SaveChangesAsync();
+            }
+
+            //check if the item to be added already got added to the basket
+            if (customerBasket.Items.Any(basketItem => basketItem.ProductId == item.ProductId))
+            {
+                //get the item and increase the count
+                var product = customerBasket.Items.FirstOrDefault(basketItem => basketItem.ProductId == item.ProductId);
+                product.Count++;
+                await _context.SaveChangesAsync();
+                return customerBasket;
+            }
+
+            //then add it to the basket
+            customerBasket.Items.Add(item);
+            await _context.SaveChangesAsync();
+
+            return customerBasket;
+        }
+
+        // DELETE: Basket/clear/1
+        [HttpPost("clear/{customerId}")]
+        public async Task<ActionResult<CustomerBasket>> ClearCustomerBasket(int customerId)
+        {
+            var customerBasket = await _context.Baskets.Include(b => b.Items).FirstOrDefaultAsync(b => b.CustomerId == customerId);
+
+            if (customerBasket == null)
+            {
+                return NotFound();
+            }
+
+            foreach (var item in customerBasket.Items)
+            {
+                _context.BasketItems.Remove(item);
+            }
+            await _context.SaveChangesAsync();
+
+            return customerBasket;
+        }
+    }
+}
---
+++ basket/basket/basket/Startup.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using basket.Data;
+using basket.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace basket
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddEntityFrameworkNpgsql().AddDbContext<BasketDbContext>(options =>
+                options.UseNpgsql(Configuration.GetConnectionString("BasketContext"))
+                //options.UseNpgsql("Host=192.168.0.234;Port=5432;Database=basketDb;Username=postgres;Password=postgres;")
+
+                );
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            using (var scope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            using (var context = scope.ServiceProvider.GetService<BasketDbContext>())
+            {
+                context.Database.EnsureCreated();
+                context.Database.Migrate();
+
+                //var item = new BasketItem
+                //{
+                //    Count = 2,
+                //    ProductName = "Hallo",
+                    
+                //};
+
+                //var basket = new CustomerBasket
+                //{
+                //    CustomerId = 1,
+                //    Items = new List<BasketItem>
+                //    {
+                //        item
+                //    }
+                //};
+
+                //context.Add(basket);
+                context.SaveChanges();
+            }
+                
+        }
+    }
+}
