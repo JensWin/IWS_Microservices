4840313cfd6abb318500a33b48b9152aec424d73
Gabriel Kolar <gabriel@kolar.info>
2020-01-29 16:49:07 +0100

    add GET method for single products

--- productcatalog/src/main/kotlin/edu/iws/productcatalog/repository/ProductRepository.kt
+++ productcatalog/src/main/kotlin/edu/iws/productcatalog/repository/ProductRepository.kt
@@ -2,7 +2,9 @@ package edu.iws.productcatalog.repository
 
 import edu.iws.productcatalog.model.Product
 import org.springframework.data.repository.CrudRepository
+import java.util.*
 
 interface ProductRepository: CrudRepository<Product, Long> {
     override fun findAll(): List<Product>;
+    override fun findById(id: Long): Optional<Product>
 }
--- productcatalog/src/main/kotlin/edu/iws/productcatalog/resource/ProductResource.kt
+++ productcatalog/src/main/kotlin/edu/iws/productcatalog/resource/ProductResource.kt
@@ -4,13 +4,20 @@ import edu.iws.productcatalog.model.Product
 import edu.iws.productcatalog.repository.ProductRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
-@RestController(value = "/product")
+@RestController
 class ProductResource(@Autowired var productRepository: ProductRepository) {
 
     @GetMapping(value = ["/"])
     fun getAllProducts(): List<Product> {
         return productRepository.findAll();
+    }
+
+    @GetMapping(value = ["/{id}"])
+    fun getProduct(@PathVariable id: Long): Optional<Product> {
+        return productRepository.findById(id);
     }
 }
